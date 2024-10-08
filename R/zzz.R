6f6377f86e4b19e8655ce8856b9a06d0ef225fa2
Steven De Keyser <u0149189@kuleuven.be>
2024-10-08 17:52:23 +0200

    initial commit

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,34 @@
-.onLoad = function(libname, pkgname){
-# install.packages("reticulate")
-cat("testFF")
-# reticulate::py_install("tensorflow", envname = "r-tensorflow", ...)
-# reticulate::use_virtualenv("r-tensorflow", required = FALSE)
-# numpy = reticulate::import("numpy")
-# scipy = reticulate::import("scipy")
+
+
+install_tensorflow = function(..., envname = "r-tensorflow") {
+
+  # Function install_tensorflow() to create a python virtual environment
+  #
+
+  reticulate::py_install("tensorflow", envname = envname, ...)
+
 }
+
+
+numpy <- NULL
+scipy <- NULL
+
+.onLoad <- function(libname, pkgname) {
+
+  cat("Welcome to the R package VecDep!")
+
+  install_tensorflow()
+
+  # Use the "r-tensorflow" environment
+
+  reticulate::use_virtualenv("r-tensorflow", required = FALSE)
+
+  # Delay load foo module (will only be loaded when accessed via $)
+
+  reticulate::py_install("numpy")
+  reticulate::py_install("scipy")
+  numpy <<- reticulate::import("numpy", delay_load = TRUE)
+  scipy <<- reticulate::import("scipy", delay_load = TRUE)
+
+}
+
