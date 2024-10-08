f1a1ebe1e287e9143f2e36250595d450158a932b
Steven De Keyser <u0149189@kuleuven.be>
2024-10-08 16:53:10 +0200

    initial commit

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onLoad = function(libname, pkgname){
+# install.packages("reticulate")
+cat("test")
+# reticulate::py_install("tensorflow", envname = "r-tensorflow", ...)
+# reticulate::use_virtualenv("r-tensorflow", required = FALSE)
+# numpy = reticulate::import("numpy")
+# scipy = reticulate::import("scipy")
+}
