7f451cdb95a6a4b526db8de289e2cfcce6a4f3c7
Steven De Keyser <u0149189@kuleuven.be>
2024-11-06 21:37:13 +0100

    initial commit

--- R/zzz.R
+++ R/zzz.R
@@ -7,12 +7,5 @@ scipy = NULL
   numpy <<- reticulate::import("numpy", delay_load = TRUE)
   scipy <<- reticulate::import("scipy", delay_load = TRUE)
 
-  if(!interactive() || stats::runif(1) > 0.1)
-    return()
-
-  welcome.message = paste0("Welcome to the R package VecDep!")
-
-  packageStartupMessage(welcome.message)
-
 }
 
